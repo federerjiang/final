9590f5ced94fc64bca8ddab6ca5a636ab3a00074
Xiaolan Jiang <federerjiang@gmail.com>
2018-12-08 18:04:14 +0900

    update

--- a2c/coordinator.py
+++ a2c/coordinator.py
@@ -66,10 +66,10 @@ def coordinator(rank, args, share_model, exp_queues, model_params):
 		count += 1
 		# if count >=15000:
 			# entropy_coef = 1
-		if count >= 1000:
-			entropy_coef = 0.5
-		if count >= 2000:
-			entropy_coef = 0.1
+		# if count >= 1000:
+			# entropy_coef = 0.5
+		# if count >= 2000:
+			# entropy_coef = 0.1
 
 		for i in range(args.num_processes):
 			model_params[i].put(model.state_dict())
