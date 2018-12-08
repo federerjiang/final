7415e7077499b296e31ed26315f9775532f76187
Xiaolan Jiang <federerjiang@gmail.com>
2018-12-08 18:00:31 +0900

    update

--- a2c/args.py
+++ a2c/args.py
@@ -7,7 +7,8 @@ class Args:
 		self.gae = 0.95
 		self.gamma = 0.99 #
 		self.tau = 1.00 #
-		self.entropy_coef = 1 #
+		self.entropy_coef = 1 # pretrain start from 1
+		# self.entropy_coef = 3 # begin from 3, then gradually reduce to 0.1
 		self.value_loss_coef = 0.5 #
 		self.max_grad_norm = 0.1 #
 		self.num_processes = 12
