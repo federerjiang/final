3bcab3e33eb5b303efde1920aeb12c114293c05b
Xiaolan Jiang <federerjiang@gmail.com>
2018-12-09 01:18:39 +0900

    update

--- env_args.py
+++ env_args.py
@@ -2,7 +2,7 @@ class EnvArgs:
 	def __init__(self):
 		# env settings
 		self.s_gop_len = 16 # gop based info
-		self.s_gop_info = 9 # or 7
+		self.s_gop_info = 7 # or 7
 		self.a_dim = 8
 		self.random_seed = 50
 		self.bitrate_levels = 4
