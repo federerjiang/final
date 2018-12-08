36f1aa1dffcbde428378c308d03c7b8b4884f735
Xiaolan Jiang <federerjiang@gmail.com>
2018-12-08 16:17:02 +0900

    update

--- a2c/coordinator.py
+++ a2c/coordinator.py
@@ -64,11 +64,11 @@ def coordinator(rank, args, share_model, exp_queues, model_params):
 	count = 0
 	while True:
 		count += 1
-		if count >=7000:
+		if count >=30000:
 			entropy_coef = 1
-		if count >= 12000:
+		if count >= 50000:
 			entropy_coef = 0.5
-		if count >= 20000:
+		if count >= 90000:
 			entropy_coef = 0.1
 
 		for i in range(args.num_processes):
