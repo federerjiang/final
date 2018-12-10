77727642620d6eeb180cd69e8484f8e74edf336b
Xiaolan Jiang <federerjiang@gmail.com>
2018-12-10 21:13:19 +0900

    update ppo

--- ppo/main.py
+++ ppo/main.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 	torch.manual_seed(args.seed)
 	
 	model = ActorCritic()
-	model.load_state_dict(torch.load('result-v3/actor.pt-4375'))
+	model.load_state_dict(torch.load('../a2c/result-v3/actor.pt-4375'))
 	model.share_memory()
 
 	# inter-process communication queues
