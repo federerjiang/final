9735c950a1a4c60edc814d08f54cbcfa52a0cf39
Xiaolan Jiang <federerjiang@gmail.com>
2018-12-12 00:35:28 +0900

    update

--- a2c/main.py
+++ a2c/main.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 	torch.manual_seed(args.seed)
 	
 	model = ActorCritic()
-	model.load_state_dict(torch.load('seletec_result/v5/actor.pt-1218'))
+	# model.load_state_dict(torch.load('seletec_result/v5/actor.pt-1218'))
 	model.share_memory()
 
 	# inter-process communication queues
